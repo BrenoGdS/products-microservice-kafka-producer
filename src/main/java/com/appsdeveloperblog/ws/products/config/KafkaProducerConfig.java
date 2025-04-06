83fc89fa6463eff35620e8de0eac925e0a8f24cb
BrenoGdS <breno.eletronica@gmail.com>
2025-04-06 16:52:03 -0300

    Fixes in Kafka Producer configuration.

--- src/main/java/com/appsdeveloperblog/ws/products/config/KafkaProducerConfig.java
+++ src/main/java/com/appsdeveloperblog/ws/products/config/KafkaProducerConfig.java
@@ -1,11 +1,13 @@
 package com.appsdeveloperblog.ws.products.config;
 
+import com.appsdeveloperblog.ws.products.event.ProductCreatedEvent;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.kafka.support.serializer.JsonSerializer;
 
@@ -18,30 +20,65 @@ public class KafkaProducerConfig {
     @Value("${spring.kafka.producer.bootstrap-servers}")
     private String bootstrapServers;
 
-    @Bean
-    public ProducerFactory<Object, Object> producerFactory() {
-        return new DefaultKafkaProducerFactory<>(producerConfigs());
-    }
+    @Value("${spring.kafka.producer.key-serializer}")
+    private String keySerializer;
+
+    @Value("${spring.kafka.producer.value-serializer}")
+    private String valueSerializer;
+
+    @Value("${spring.kafka.producer.enable.idempotence}")
+    private boolean enableIdempotence;
+
+    @Value("${spring.kafka.producer.acks}")
+    private String acks;
+
+    @Value("${spring.kafka.producer.retries}")
+    private int retries;
+
+    @Value("${spring.kafka.producer.max.in.flight.requests.per.connection}")
+    private int maxInFlightRequestsPerConnection;
+
+    @Value("${spring.kafka.producer.properties.retry.backoff.ms}")
+    private int retryBackoffMs;
+
+    @Value("${spring.kafka.producer.properties.delivery.timeout.ms}")
+    private int deliveryTimeoutMs;
+
+    @Value("${spring.kafka.producer.properties.linger.ms}")
+    private int lingerMs;
+
+    @Value("${spring.kafka.producer.properties.request.timeout.ms}")
+    private int requestTimeoutMs;
 
     @Bean
     public Map<String, Object> producerConfigs() {
         Map<String, Object> props = new HashMap<>();
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class); // Serializer for keys (converts keys from String to byte array)
-        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class); // Serializer for values (converts JSON to byte array)
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, keySerializer);  // Serializer for keys (converts keys from String to byte array)
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, valueSerializer);  // Serializer for values (converts JSON to byte array)
 
         // Enable idempotence and strong durability
-        props.put(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, true); // Prevents duplicate messages during retries.
-        props.put(ProducerConfig.ACKS_CONFIG, "all"); // Wait for acknowledgements from all in-sync replicas for strong durability
-        props.put(ProducerConfig.RETRIES_CONFIG, Integer.MAX_VALUE); // Number of retry attempts if message sending fails (adjust as needed)
-        props.put(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, 5); // Maximum number of unacknowledged requests per connection (must be ≤ 5 for idempotence)
+        props.put(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, enableIdempotence);  // Prevents duplicate messages during retries.
+        props.put(ProducerConfig.ACKS_CONFIG, acks);  // Wait for acknowledgements from all in-sync replicas for strong durability
+        props.put(ProducerConfig.RETRIES_CONFIG, retries);  // Number of retry attempts if message sending fails (adjust as needed)
+        props.put(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, maxInFlightRequestsPerConnection);  // Maximum number of unacknowledged requests per connection (must be ≤ 5 for idempotence)
 
         // Additional properties:
-        props.put(ProducerConfig.RETRY_BACKOFF_MS_CONFIG, 1000);      // Wait 1 second between retries
-        props.put(ProducerConfig.DELIVERY_TIMEOUT_MS_CONFIG, 120000);   // 2 minutes total delivery timeout
-        props.put(ProducerConfig.LINGER_MS_CONFIG, 0);                  // Send immediately (no delay)
-        props.put(ProducerConfig.REQUEST_TIMEOUT_MS_CONFIG, 30000);     // Wait up to 30 seconds for a broker response
+        props.put(ProducerConfig.RETRY_BACKOFF_MS_CONFIG, retryBackoffMs);  // Wait 1 second between retries
+        props.put(ProducerConfig.DELIVERY_TIMEOUT_MS_CONFIG, deliveryTimeoutMs);  // 2 minutes total delivery timeout
+        props.put(ProducerConfig.LINGER_MS_CONFIG, lingerMs);  // Send immediately (no delay)
+        props.put(ProducerConfig.REQUEST_TIMEOUT_MS_CONFIG, requestTimeoutMs);  // Wait up to 30 seconds for a broker response
 
         return props;
+    }
+
+    @Bean
+    public KafkaTemplate<String, ProductCreatedEvent> kafkaTemplate() {
+        return new KafkaTemplate<String, ProductCreatedEvent>(producerFactory());
+    }
+
+    @Bean
+    public ProducerFactory<String, ProductCreatedEvent> producerFactory() {
+        return new DefaultKafkaProducerFactory<>(producerConfigs());
     }
 }
